9245033f5d70715e48b5e493c41790056320cd86
Matthias Hermann <matthias.hermann@iteratec.de>
2018-04-08 12:01:21 +0200

    Read queue data strings from console in run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,15 @@
 sudo docker stop maild
 sudo docker rm maild
 
+echo "Please enter the Queue name for this instance: "
+read QUEUE_NAME
+echo
+echo "Please enter the connection string for this instance: "
+read CONNECTION_STRING
+
 sudo docker run -d -it \
     -p 1337:1337 \
+    -e QUEUE_NAME="${QUEUE_NAME}" \
+    -e CONNECTION_STRING="${CONNECTION_STRING}" \
     --name=uploadd \
     upload_service
